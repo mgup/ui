2e881ac4a7507e4a4fede08a1e347ee01f5b6315
Vitaly Shlyaga <storkvist@storkvist.net>
2013-05-18 22:11:17 +0400

    ui

--- lib/ui/engine.rb
+++ lib/ui/engine.rb
@@ -1,7 +1,9 @@
 module Ui
-  class Engine < ::Rails::Engine
-    initializer 'ui.load_static_assets' do |app|
-      app.middleware.use ::ActionDispatch::Static, "#{root}/vendor"
+  module Rails
+    class Engine < ::Rails::Engine
+      #initializer 'ui.load_static_assets' do |app|
+      #  app.middleware.use ::ActionDispatch::Static, "#{root}/vendor"
+      #end
     end
   end
 end
