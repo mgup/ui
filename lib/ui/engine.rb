3b7c40d696d5d4551da14298624a0ede51f75fec
Vitaly Shlyaga <storkvist@storkvist.net>
2013-05-18 23:09:41 +0400

    ui

--- lib/ui/engine.rb
+++ lib/ui/engine.rb
@@ -1,10 +1,10 @@
 module Ui
-  module Rails
+  #module Rails
     class Engine < ::Rails::Engine
       #initializer :assets do |app|
       #  Rails.application.config.assets.precompile
       #  app.middleware.use ::ActionDispatch::Static, "#{root}/vendor"
       #end
     end
-  end
+  #end
 end
