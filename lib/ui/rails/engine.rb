37acd4be12d9e7bc0e6d403fb525d0c01e5e3c19
Vitaly Shlyaga <storkvist@storkvist.net>
2013-05-21 11:21:02 +0400

    Порядок загрузки стилей.

--- lib/ui/rails/engine.rb
+++ lib/ui/rails/engine.rb
@@ -1,11 +1,6 @@
 module Ui
   module Rails
     class Engine < ::Rails::Engine
-      initializer 'ui.assets.precompile', :group => :all do |app|
-        app.config.assets.precompile += %w[
-          store/all.*
-        ]
-      end
     end
   end
 end
