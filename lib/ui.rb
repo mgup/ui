f3e896da6aedb31b7ce7a7371a689f606de62e0c
Vitaly Shlyaga <storkvist@storkvist.net>
2013-05-27 16:11:14 +0400

    javascript

--- lib/ui.rb
+++ lib/ui.rb
@@ -5,5 +5,7 @@ module Ui
 end
 
 require 'simple-navigation'
-require 'ui/sidebar_renderer'
+require 'ui/navigation_renderers/navbar_renderer'
+require 'ui/navigation_renderers/sidebar_renderer'
+SimpleNavigation.register_renderer navbar: NavbarRenderer
 SimpleNavigation.register_renderer sidebar: SidebarRenderer
